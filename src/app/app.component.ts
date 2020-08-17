8557e4cb1d2bce1c99bbdb45c5f786590a27aeb0
Irina Vasilescu <irinavasilescu@192-168-0-104.rdsnet.ro>
2020-08-17 21:21:50 +0300

    light theme/dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UiStyleToggleService } from './services/ui-style-toggle.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private uiStyleToggleService: UiStyleToggleService
+  ) {}
+
   title = 'portfolio';
+
+  toggleTheme() {
+    this.uiStyleToggleService.toggle();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MaterialModule } from '../app/material/material.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { UiStyleToggleService } from "./services/ui-style-toggle.service";
+import { StorageService } from "./services/storage.service";
+
+export function themeFactory(themeService: UiStyleToggleService) {
+	return () => themeService.setThemeOnStart();
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		WelcomeComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		NgbModule,
+		MaterialModule
+	],
+	providers: [
+		UiStyleToggleService,
+		StorageService,
+		{provide: APP_INITIALIZER, useFactory: themeFactory, deps: [UiStyleToggleService], multi: true},
+	],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
