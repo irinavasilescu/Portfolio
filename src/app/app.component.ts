7297ba7c2e72e09c4713950c6df4a914d68bb6d8
Irina Vasilescu <irinavasilescu@192-168-0-104.rdsnet.ro>
2020-08-17 22:14:38 +0300

    dark mode/light mode works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { UiStyleToggleService } from './services/ui-style-toggle.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
