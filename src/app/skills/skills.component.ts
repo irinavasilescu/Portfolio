132e9424670ce3786d94baadc192838f108f8829
Irina Vasilescu <irinavasilescu@192-168-0-104.rdsnet.ro>
2020-08-19 17:54:07 +0300

    added second image

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Label, SingleDataSet } from 'ng2-charts';
-import { ChartType } from 'chart.js';
 import { UiStyleToggleService } from '../services/ui-style-toggle.service';
 
 export enum ThemeMode {
-	LIGHT,
-	DARK
+	DARK, LIGHT
 }
 
 @Component({
@@ -25,17 +22,4 @@ export class SkillsComponent implements OnInit {
 	isDarkTheme() {
 		return this.uiStyleToggleService.theme$.value === ThemeMode.DARK;
 	}
-
-	public donutColors = [
-		{
-			backgroundColor: [
-				'#ced',
-				'#fda',
-				'#fdd',
-			]
-		}
-	];
-	public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-	public doughnutChartData: SingleDataSet = [1, 1, 1];
-	public doughnutChartType: ChartType = 'doughnut';
 }
