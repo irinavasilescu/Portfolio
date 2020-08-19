a7d99bfae9c10652898cc6e0655a391f19e3d80e
Irina Vasilescu <irinavasilescu@192-168-0-104.rdsnet.ro>
2020-08-19 16:22:39 +0300

    added buttons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 const MaterialModules = [
 	MatToolbarModule,
-	MatIconModule
+	MatIconModule,
+	MatButtonModule
 ];
 
 @NgModule({
