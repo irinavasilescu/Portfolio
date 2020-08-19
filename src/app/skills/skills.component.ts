71cb2c9ed839bddb947bdfa3a2c3d58b541b384a
Irina Vasilescu <irinavasilescu@192-168-0-104.rdsnet.ro>
2020-08-19 16:46:51 +0300

    color transition for 'skills'

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,31 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Label, SingleDataSet } from 'ng2-charts';
 import { ChartType } from 'chart.js';
+import { UiStyleToggleService } from '../services/ui-style-toggle.service';
+
+export enum ThemeMode {
+	LIGHT,
+	DARK
+}
 
 @Component({
-  selector: 'skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+	selector: 'skills',
+	templateUrl: './skills.component.html',
+	styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		public uiStyleToggleService: UiStyleToggleService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  public donutColors = [
+	isDarkTheme() {
+		return this.uiStyleToggleService.theme$.value === ThemeMode.DARK;
+	}
+
+	public donutColors = [
 		{
-		  backgroundColor: [
-			'#ced',
-			'#fda',
-			'#fdd',
-		  ]
+			backgroundColor: [
+				'#ced',
+				'#fda',
+				'#fdd',
+			]
 		}
 	];
 	public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
 	public doughnutChartData: SingleDataSet = [
-	  1, 1, 1
+		1, 1, 1
 	];
 	public doughnutChartType: ChartType = 'doughnut';
 
