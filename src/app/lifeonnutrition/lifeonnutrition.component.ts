b4bae8c4adfbab8a2c02a77924d560c63d9c4d0d
Irina Vasilescu <irina>
2020-10-13 19:10:59 +0300

    some aligning and added video to lifeonnutrition

--- src/app/lifeonnutrition/lifeonnutrition.component.ts
+++ src/app/lifeonnutrition/lifeonnutrition.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-lifeonnutrition',
-  templateUrl: './lifeonnutrition.component.html',
-  styleUrls: ['./lifeonnutrition.component.css', './../app.component.scss']
+	selector: 'app-lifeonnutrition',
+	templateUrl: './lifeonnutrition.component.html',
+	styleUrls: ['./lifeonnutrition.component.css', './../app.component.scss']
 })
 export class LifeonnutritionComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		const tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/iframe_api";
+		document.body.appendChild(tag);
+	}
 
 }
