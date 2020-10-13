829c0e40a8f4117a49b743cc88ec680e38672924
Irina Vasilescu <irina>
2020-10-14 00:27:03 +0300

    close dialog

--- src/app/shelves/shelves.component.ts
+++ src/app/shelves/shelves.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
 	selector: 'shelves',
@@ -11,10 +12,15 @@ export class ShelvesComponent implements OnInit {
 	displayURL: any;
 
 	constructor(
+		public dialogRef: MatDialogRef<any>,
 		private sanitizer: DomSanitizer
 	) { }
 
 	ngOnInit(): void {
 		this.displayURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/XzySnWsAhWU');
+	}
+
+	closeDialog() {
+		this.dialogRef.close();
 	}
 }
